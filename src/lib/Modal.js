491ef0bc85048ed5396e8b0e4c18d4b8c54bd8e4
Reda M <reda.madjoub@gmail.com>
2022-01-17 10:28:32 +0100

    update package.json

--- src/lib/Modal.js
+++ src/lib/Modal.js
@@ -9,17 +9,16 @@ export default function Modal(props) {
     }
     return (
         <>
-        {
-            !open && (
-                <aside className='background-Modal'>
-                    <div className='container-Modal'>
-                        <button className="button-Modal" onClick={closeModal}>&#x2715;</button>
-                        <h3>{content}</h3>
-                    </div>
-                </aside>
-
-            )
-        }
+            {
+                !open && (
+                    <aside className='background-Modal'>
+                        <div className='container-Modal'>
+                            <button className="button-Modal" onClick={closeModal}>&#x2715;</button>
+                            <h3>{content}</h3>
+                        </div>
+                    </aside>
+                )
+            }
         </>
     )
 }
